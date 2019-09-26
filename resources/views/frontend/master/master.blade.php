f5f28e47b7207bc0e28f49d69d898e2dbdcea8fb
= <=>
2019-09-26 16:04:03 +0700

    finish frontend master pages

--- resources/views/frontend/master/master.blade.php
+++ resources/views/frontend/master/master.blade.php
@@ -4,7 +4,7 @@
 <head>
 	<meta charset="utf-8">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<title>VIETPRO STORE</title>
+	<title>@yield('title')</title>
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 
 	<base href="{{ asset('frontend') }}/">
